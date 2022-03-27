c527363617ad1dc2ce21fe35372229fcdbdeac22
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2022-03-27 17:30:01 -0300

    Update index.js
    * Added a condition in the 'newCard()' function, which verifies whether the player can receive a new card; * Created an object called a 'player', which will store the player's information; * The code is more documented

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+// Points of the Player
 let sum = 0
+
+// Array for the cards
 let cards = []
+
+// Player Conditions
 let hasBlackJack = false
 let isAlive = false
+
+// Variable to show the player's situation
 let message = ""
+
+// Variables to change the html document using DOM
 let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.querySelector("#sum-el")
-// console.log(messageEl)
 
+// Player's Information
+let player = {
+    name: 'Matheus',
+    chips: 200
+}
+
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = `${player.name}: $${player.chips}`
+// Function to get a random card
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() * 13) + 1
     if (randomNumber > 10){
@@ -19,6 +37,7 @@ function getRandomCard(){
     }
 }
 
+// Function to start the game
 function startGame(){
     isAlive = true
     // First and Second Cards
@@ -29,6 +48,7 @@ function startGame(){
     renderGame()
 }
 
+// Function to show informations about the game to the player
 function renderGame(){
     cardsEl.textContent = "Cards: "
     for(let i =0; i< cards.length; i++){
@@ -40,16 +60,27 @@ function renderGame(){
         message = "Do you want to draw a new card ?"
     } else if(sum == 21){
         message = "You've got a Blackjack!"
+        hasBlackJack = true
     } else{
         message = "You loose!"
         isAlive = false
     }
+    // Displays the message
     messageEl.textContent = message
 }
 
+// Condition to check if the player can get a new card
+// If it can, the function 'new card()' it's called
+if(isAlive && !hasBlackJack){
+    newCard()
+}
+
+// Function to get a new card
 function newCard(){
-    let newCard = getRandomCard()
-    cards.push(newCard)
-    sum = sum + newCard
-    renderGame()
+    if(isAlive && !hasBlackJack){
+        let newCard = getRandomCard()
+        cards.push(newCard)
+        sum += newCard
+        renderGame()
+    }
 }
